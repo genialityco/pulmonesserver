4cf03edb0ec8270d93a1b00b3a248ff80c067c49
balderban <balderban@gmail.com>
2024-09-25 17:56:33 -0500

    first commit

---
+++ src/rooms/MyRoom.ts
@@ -0,0 +1,69 @@
+import { Room, Client } from "@colyseus/core";
+import { MyRoomState,Player,Item } from "./schema/MyRoomState";
+
+const FLOOR_SIZE = 4;
+const CELLS_NUMBER = 10;
+export class MyRoom extends Room<MyRoomState> {
+  maxClients = 4;
+  
+
+  onCreate (options: any) {
+    this.setState(new MyRoomState());
+
+    for (let i=0;i<CELLS_NUMBER;i++){
+
+      const item = new Item();  
+      item.x = -(FLOOR_SIZE/2) + (Math.random() * FLOOR_SIZE);
+      item.y = 1.031;
+      item.z = -(FLOOR_SIZE/2) + (Math.random() * FLOOR_SIZE);
+
+      this.state.items.set(String(i), item);
+    }
+
+
+    this.onMessage("type", (client, message) => {
+      //
+      // handle "type" message
+      //
+    });
+
+    this.onMessage("updatePosition", (client, data) => {
+      const player = this.state.players.get(client.sessionId);
+      player.x = data.x;
+      player.y = data.y;
+      player.z = data.z;
+    }); 
+    
+    this.onMessage("selectedItem", (client, data) => {
+      console.log('selectedItem',data.itemID);
+      delete this.state.items[data.itemID];
+    });   
+
+  }
+
+  onJoin(client: Client, options: any) {
+    console.log(client.sessionId, "joined!");
+
+    // create Player instance
+    const player = new Player();
+
+    // place Player at a random position
+    player.x = -(FLOOR_SIZE/2) + (Math.random() * FLOOR_SIZE);
+    player.y = 1.031;
+    player.z = -(FLOOR_SIZE/2) + (Math.random() * FLOOR_SIZE);
+
+    // place player in the map of players by its sessionId
+    // (client.sessionId is unique per connection!)
+    this.state.players.set(client.sessionId, player);
+}
+
+  onLeave (client: Client, consented: boolean) {
+    console.log(client.sessionId, "left!");
+    this.state.players.delete(client.sessionId);
+  }
+
+  onDispose() {
+    console.log("room", this.roomId, "disposing...");
+  }
+
+}
---
+++ src/rooms/schema/MyRoomState.ts
@@ -0,0 +1,21 @@
+// MyRoomState.ts
+import { MapSchema, Schema, type } from "@colyseus/schema";
+
+export class Player extends Schema {
+    @type("number") x: number;
+    @type("number") y: number;
+    @type("number") z: number;
+}
+
+export class Item extends Schema {
+  @type("number") x: number;
+  @type("number") y: number;
+  @type("number") z: number;
+}
+
+
+
+export class MyRoomState extends Schema {
+  @type({ map: Player }) players = new MapSchema<Player>();
+  @type({ map: Item }) items = new MapSchema<Item>();
+}
